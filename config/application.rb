8e11ebe10e92f712902c6830cfc915af78bd13d1
EC2 Default User <ec2-user@ip-172-31-4-101.us-east-2.compute.internal>
2020-08-27 05:15:51 +0000

    mysql

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,9 @@ module MessageBoard
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    
+    
+    config.hosts << '.amazonaws.com'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
