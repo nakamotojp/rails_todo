3e474cec716e3af85956da1b0bee89417052809a
EC2 Default User <ec2-user@ip-172-31-4-101.us-east-2.compute.internal>
2020-08-27 05:54:46 +0000

    message bord all

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+#  get 'messages/:id', to: 'messages#show'
+#  get 'messages', to: 'messages#index'
+
+#  post 'messages', to: 'messages#create'
+#  get 'messages/new', to: 'messages#new'
+
+#  put 'messages/:id', to: 'messages#udpate'
+#  get 'messages/:id/edit', to: 'messages#edit'
+
+
+#  delete 'messages/:id', to: 'messages#destroy'
+root to: 'messages#index'
+resources :messages
 end
